ae5c966512c8be49116a703501ad1f1f9498795d
a-s-h11 <ashmit.baranwal@s.amity.edu>
2025-05-06 11:16:27 +0530

    master commit

---
+++ file.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	char a[50], b;
+	int c;
+	printf("enter your name: ");
+	scanf("%s", &a);
+	printf("enter your gender (m/f/o): ");
+	scanf("%c", &b);
+	printf("enter your age: ");
+	scanf("%d", &c);
+}
+
